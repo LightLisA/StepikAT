a3388875672b1ae73ec8fe461278f0bfc91a48cb
nenormalnuy@ukr.net <olly1215@netcracker.com>
2019-11-05 19:01:39 +0200

    added file 3..2.4 UPDATE

--- 3.2.4(last).py
+++ 3.2.4(last).py
@@ -29,9 +29,11 @@ class TestStepik(unittest.TestCase):
 
 		# Проверяем, что смогли зарегистрироваться
 		# ждем загрузки страницы
+		НАДО ПЕРЕДЕЛАТЬ НА КОМАНДУ ОТКЛИКА СТРАНИЦЫ
 		time.sleep(2)
 
 		# находим элемент, содержащий текст
+		УДАЛИТЬ, ИСПОЛЬЗУЕМ АСЕРТ НОВЫЙ
 		welcome_text_elt = browser.find_element_by_tag_name("h1")
 		# записываем в переменную welcome_text текст из элемента welcome_text_elt
 		welcome_text = welcome_text_elt.text
