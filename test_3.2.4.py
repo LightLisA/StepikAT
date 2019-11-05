07b54889363ae3805f88f67fb672e2153f427771
Aleksey <nenormalnuy@ukr.net>
2019-11-06 01:52:37 +0200

    don't run

---
+++ test_3.2.4.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+import unittest
+import time
+
+class TestStepik(unittest.TestCase):
+	def test_form_1():
+
+		f1_link = "http://suninjuly.github.io/get_attribute.html"
+		browser = webdriver.Chrome()
+		browser.get(link)
+
+		# Ваш код, который заполняет обязательные поля
+		f1_name = browser.find_element_by_css_selector(".first_block .first")
+		f1_name.send_keys("Rozalyn")
+		f1_lastname =  browser.find_element_by_css_selector(".first_block .second")
+		f1_lastname.send_keys("Krozenstaein")
+		f1_mail = browser.find_element_by_css_selector(".first_block .third")
+		f1_mail.send_keys("roz.kroz@mailer.com")
+
+		# Ваш код, который заполняет не обязательные поля
+		f1_phone = browser.find_element_by_css_selector(".second_block .first")
+		f1_phone.send_keys("+1659456789809")
+		f1_address = browser.find_element_by_css_selector(".second_block .second")
+		f1_address.send_keys("UNIT 2 150 NIAGARA LAKE ONTARIO")
+
+		# Отправляем заполненную форму
+		f1_button = browser.find_element_by_css_selector("button.btn")
+		f1_button.click()
+
+		# Проверяем, что смогли зарегистрироваться
+		# ждем загрузки страницы
+		f1_browser.implicitly_wait(10)		
+
+		# находим элемент, содержащий текст
+		f1_welcome_text_elt = browser.find_element_by_tag_name("h1")
+		# записываем в переменную welcome_text текст из элемента welcome_text_elt
+		f1_welcome_text = f1_welcome_text_elt.text
+
+		# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+		f1_ER = 'Congratulations! You have successfully registered!'
+		assertEquals(f1_ER, f1_welcome_text, "First ER is not match")
+
+
+	def test_form_2():
+		f2_link = "http://suninjuly.github.io/registration2.html"
+		f2_browser = webdriver.execute_script("window.open(f2_link)")
+
+		# заполняет обязательные поля
+		f2_name = f2_browser.find_element_by_css_selector(".first_block .first")
+		f2_name.send_keys("Rosa")
+		
+		f2_lastname = f2_browser.find_element_by_css_selector(".first_block .second")
+		f2_lastname.send_keys("Willyams")
+
+		f2_email = find_element_by_css_selector(".first_block .third")
+		f2_email.send_keys("rosa.wil@cogery.com")
+
+		# Заполняет не обязательные поля
+		f2_phone = find_element_by_css_selector(".second_block .first")
+		f2_phone.send_keys("+875456212001")
+
+		f2_address = find_element_by_css_selector(".second_block .second")
+		f2_address.send_keys("42 SYSCON ROAD WILLYAMS")
+
+		# НАжимаем Отправить форму
+		f2_button = f2_browser.find_element_by_css_selector("button.btn")
+		f2_button.click()
+
+		# Проверяем, что смогли зарегистрироваться
+		# ждем загрузки страницы
+		f1_browser.implicitly_wait(10)
+
+		# находим элемент, содержащий текст
+		f2_text_elm = f2_browser.find_element_by_css_selector(".container h1")
+		f2_text = f2_text_elm.text
+
+		# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+		f2_ER = 'Congratulations! You have successfully registered!'
+		assertEquals(f2_ER, f2_welcome_text, "Second ER is not match")
+
+		assertEquals(f1_ER, f2_ER, "ERs are not match")
+
+
+		# ждем чтобы взглянуть
+		time.sleep(10)
+
+		# закрываем
+		browser.quit()
+
+		
